e7a08a1a067517b013bfac2a886b093d5cf453f6
RConradG <RCGadduang@gmail.com>
2025-03-29 16:53:41 -0700

    wip: shows ingredients added to burger stack, added remove from burger functionality

--- src/components/BurgerStack/BurgerStack.jsx
+++ src/components/BurgerStack/BurgerStack.jsx
@@ -1,3 +1,18 @@
 const BurgerStack = (props) => {
-  return <ul>// map through props.ingredients</ul>
+  return (
+    <ul>
+      {props.stack.map( (ingredient, index) => {
+        return(
+          <li style={{ backgroundColor: ingredient.color}} key={index}>
+            {ingredient.name}
+            <button onClick={ () => props.removeFromBurger(ingredient) }>
+              X
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
+
+export default BurgerStack;
